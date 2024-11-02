7aeb0350d23e2c9ba111815251c01110b3bb55f1
Lucca Salles <luccasalles1@gmail.com>
2024-11-02 16:46:45 -0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -6,28 +6,25 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-    // Seleciona todos os links do menu
     $('#nav_list a, #mobile_nav_list a').on('click', function(event) {
-        // Previne o comportamento padrão do link
+    
         event.preventDefault();
 
-        // Obtém o destino do link
+      
         var target = $(this).attr('href');
 
-        // Rola suavemente até o destino
+
         $('html, body').animate({
             scrollTop: $(target).offset().top
-        }, 800); // 800 milissegundos para a animação
+        }, 800); 
     });
 });
 
 $(document).ready(function() {
-    // Ao clicar no botão "Leia Avaliações"
-    $('#btn-read-reviews').on('click', function() {
-        // Rolagem suave para a seção de avaliações
+    $('#btn-read-reviews').on('click', function() {s
         $('html, body').animate({
             scrollTop: $('#testimonials').offset().top
-        }, 800); // 800 é a duração da animação em milissegundos
+        }, 800);
     });
 });
 
